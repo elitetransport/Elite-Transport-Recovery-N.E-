f0f84e43b1b47eee3ac1c68389780b0aa2e23832
elitetransport <lee_meredith8@hotmail.com>
2026-05-09 10:16:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function calculateQuote() {
 let miles = document.getElementById('miles').value;
 let total = miles * 2.5;
 document.getElementById('result').innerHTML =
-'Estimated Price: £' 80 + total.toFixed(2);
+'Estimated Price: £' + total.toFixed(2);
 }
